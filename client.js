4efbe3d7f484107c25479633e004bfb5c53ee8f3
MohammedMahdi21 <mmd.mahdi21@gmail.com>
2022-04-21 23:56:30 +0000

    updating code for better Readability

--- client.js
+++ client.js
@@ -1,6 +1,5 @@
 const net = require("net");
 
-
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
@@ -14,9 +13,9 @@ const connect = function () {
   })
 
   // Update the connect function to handle incoming data and console.log it for the player.
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-  });
+  //conn.on('data', (data) => {
+  //  console.log('Server says: ', data);
+  //});
 
   // interpret incoming data as text
   conn.setEncoding("utf8");

d31418b13e190907fd1389cfd6720a49ac3c84ac
MohammedMahdi21 <mmd.mahdi21@gmail.com>
2022-04-22 00:03:30 +0000

    adding short chat bubbles to be displayed on top of the snake

--- client.js
+++ client.js
@@ -12,10 +12,15 @@ const connect = function () {
     conn.write("Name: Moe");
   })
 
+// to display text, short chat bubbles
+  conn.on('connect', () =>{
+    conn.write("Say: Hello All");
+  })
+
   // Update the connect function to handle incoming data and console.log it for the player.
-  //conn.on('data', (data) => {
-  //  console.log('Server says: ', data);
-  //});
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");

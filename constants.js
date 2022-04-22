a6a87c3927a745e0e74419c9d581222b7b0c896b
MohammedMahdi21 <mmd.mahdi21@gmail.com>
2022-04-22 20:33:40 +0000

    creating constants to have all (static) values in one configuration file to be easily tweaked.

---
+++ constants.js
@@ -0,0 +1,17 @@
+const IP = '10.0.2.15';
+const PORT = '50541';
+const MOVE_UP_KEY = "Move: up";
+const MOVE_DOWN_KEY = "Move: down";
+const MOVE_LEFT_KEY = "Move: left";
+const MOVE_RIGHT_KEY = "Move: right";
+const PLAYER_NAME = "Name: Moe"
+
+module.exports = {
+  IP,
+  PORT,
+  MOVE_UP_KEY,
+  MOVE_DOWN_KEY,
+  MOVE_LEFT_KEY,
+  MOVE_RIGHT_KEY,
+  PLAYER_NAME
+};
